3889cba3ac53ecd707548a45ccc97857aa6393be
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-17 11:18:07 +0700

    poin2 aplikasi bank transfer dengan runtime configuration

---
+++ BankTransferConfig.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace modul8_103022300114
+{
+    class BankTransferConfig {
+        public static string filePath = @"BankTransferConfig.json";
+        public string lang { get; set; }
+        public Transfer transfer { get; set; }
+        public string[] methods { get; set; }
+        public Confirmation confirmation { get; set; }
+
+        public class Transfer {
+            public int threshold { get; set; }
+            public int low_fee { get; set; }
+            public int high_fee { get; set; }
+
+        }
+
+        public class Confirmation {
+            public string en { get; set; }
+            public string id { get; set; }
+        }
+
+        public BankTransferConfig(string lang, Transfer transfer, string[] methods, Confirmation confirmation)
+        {
+            this.lang = lang;
+            this.transfer = transfer;
+            this.methods = methods;
+            this.confirmation = confirmation;
+        }
+
+        public BankTransferConfig()
+        {
+
+        }
+
+        public BankTransferConfig ReadConfigFile()
+        {
+            String configJsonData = File.ReadAllText(filePath);
+            BankTransferConfig data = JsonSerializer.Deserialize<BankTransferConfig>(configJsonData);
+            return data;
+        }
+    } 
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics.SymbolStore;
+using modul8_103022300114;
+
+class Program 
+{
+    public static void Main(String[] args) {
+        BankTransferConfig config = new BankTransferConfig();
+        BankTransferConfig test = config.ReadConfigFile();
+
+        //poin1
+        if (test.lang == "en")
+        {
+            Console.WriteLine("Please insert the amount of money to transfer:");
+        }
+        else if (test.lang == "id")
+        {
+            Console.WriteLine("Silahkan masukkan jumlah uang yang akan ditransfer:");
+        }
+
+        //poin2
+        int money = Convert.ToInt32(Console.ReadLine());    
+        if (money <= test.transfer.threshold)
+        {
+            Console.WriteLine("Please select the transfer method:");
+            for (int i = 0; i < test.methods.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {test.methods[i]}");
+            }
+            int method = Convert.ToInt32(Console.ReadLine());
+            if (method == 1)
+            {
+                Console.WriteLine($"The transfer fee is {test.transfer.low_fee}");
+            }
+            else if (method == 2)
+            {
+                Console.WriteLine($"The transfer fee is {test.transfer.high_fee}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid amount");
+        }
+
+        //poin3
+
+
+    }
+}
